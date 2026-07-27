79d5a8bd949f6d560a48c794ef7ae63f1b8b3300
GamavOfficial <gamavgaming@gmail.com>
2026-07-27 10:30:04 +0530

    Clean up player.js code structure
    Refactor player.js by removing unnecessary comments and whitespace.

--- js/player.js
+++ js/player.js
@@ -1,7 +1,7 @@
-// ===============================
+// ===============================================
 // Rubik Solver Pro
-// player.js
-// ===============================
+// player.js (Complete Code)
+// ===============================================
 
 let currentStep = 0;
 let moves = [];
@@ -10,101 +10,72 @@ let moves = [];
  * Load solution moves
  */
 function loadSolution(solution) {
-
     moves = Array.isArray(solution) ? solution : [];
-
     currentStep = 0;
-
 }
 
 /**
  * Get current step index
  */
 function getCurrentStep() {
-
     return currentStep;
-
 }
 
 /**
  * Get total steps
  */
 function getTotalSteps() {
-
     return moves.length;
-
 }
 
 /**
  * Get current move
  */
 function getCurrentMove() {
-
     if (moves.length === 0)
         return null;
 
     return moves[currentStep];
-
 }
 
 /**
  * Next step
  */
 function nextStep() {
-
     if (currentStep < moves.length - 1) {
-
         currentStep++;
-
     }
-
     return getCurrentMove();
-
 }
 
 /**
  * Previous step
  */
 function previousStep() {
-
     if (currentStep > 0) {
-
         currentStep--;
-
     }
-
     return getCurrentMove();
-
 }
 
 /**
  * First step?
  */
 function isFirstStep() {
-
     return currentStep === 0;
-
 }
 
 /**
  * Last step?
  */
 function isLastStep() {
-
     return currentStep === moves.length - 1;
-
 }
 
 /**
  * Reset player
  */
 function resetPlayer() {
-
     currentStep = 0;
-
     moves = [];
-
 }
-
-
-
